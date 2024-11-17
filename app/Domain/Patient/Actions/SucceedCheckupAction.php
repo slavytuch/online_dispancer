6929a8cd07b9a519485c98ae371dd333941d6a08
Viacheslav Luchko <luchko@ittest-team.ru>
2024-11-17 11:59:53 +0300

    Another bug

--- app/Domain/Patient/Actions/SucceedCheckupAction.php
+++ app/Domain/Patient/Actions/SucceedCheckupAction.php
@@ -7,7 +7,7 @@ use App\Models\Checkup;
 
 class SucceedCheckupAction
 {
-    public function execute(Checkup $checkup, mixed $result)
+    public function execute(Checkup $checkup, $result = null)
     {
         $checkup->status = CheckupStatus::Finished;
         $checkup->checkup_data = $result;
